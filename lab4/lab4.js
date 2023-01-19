5237cce442a2ab1768cfc3a765cac3bb493dc613
Siddh Bhalodia <97431263+siddh2604@users.noreply.github.com>
2023-01-19 12:18:57 +0530

    Add files via upload

---
+++ lab4/lab4.js
@@ -0,0 +1,33 @@
+const a = document.getElementById()
+const bike={
+    company:"TRIUMPH",
+    model:"Bobber",
+    price:"₹8 lac",
+    color:"BLACK",
+    type:"BIKE",
+}
+const car={
+    company:"FORD",
+    model:"Mustang GT", 
+    price:"₹68 lac",
+    color:"BLACK",
+    type:"CAR",
+}
+const mobile={
+    company:"Samsung",
+    model:"Galaxy S22 Ultra", 
+    price:"₹1.2 lac",
+    color:"BLACK",
+    type:"SMARTPHONE",
+}
+function call(value) {
+    switch (value) {
+        case "car":
+            for(i in car)
+            {
+
+            }
+        case "bike":
+        case "mobile":
+    }
+}
