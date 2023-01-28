ec8640601c28ff17d85caaf9c916a5239f9cb9f9
Siddh Bhalodia <97431263+siddh2604@users.noreply.github.com>
2023-01-28 15:12:18 +0530

    Add files via upload

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -1,33 +1,71 @@
-const a = document.getElementById()
-const bike={
-    company:"TRIUMPH",
-    model:"Bobber",
-    price:"₹8 lac",
-    color:"BLACK",
-    type:"BIKE",
+
+const bike = {
+    company: "TRIUMPH",
+    model: "Bobber",
+    price: "₹8 lac",
+    color: "BLACK",
+    type: "BIKE",
 }
-const car={
-    company:"FORD",
-    model:"Mustang GT", 
-    price:"₹68 lac",
-    color:"BLACK",
-    type:"CAR",
+const car = {
+    company: "FORD",
+    model: "Mustang GT",
+    price: "₹68 lac",
+    color: "BLACK",
+    type: "CAR",
 }
-const mobile={
-    company:"Samsung",
-    model:"Galaxy S22 Ultra", 
-    price:"₹1.2 lac",
-    color:"BLACK",
-    type:"SMARTPHONE",
+const mobile = {
+    company: "Samsung",
+    model: "Galaxy S22 Ultra",
+    price: "₹1.2 lac",
+    color: "BLACK",
+    type: "SMARTPHONE",
 }
+value = "";
 function call(value) {
+    console.log(value);
+    
     switch (value) {
         case "car":
-            for(i in car)
-            {
+            for (i in car) {
+                tr = document.createElement("tr");
+                const trkey = document.createElement("td");
+                const rvalue = document.createElement("td");
+                trkey.textContent = i;
+                rvalue.textContent = car[i];
 
+                tr.appendChild(trkey);
+                tr.appendChild(rvalue);
+
+                document.getElementById("id1").appendChild(tr);
             }
+            break;
         case "bike":
+            for (i in bike) {
+                tr = document.createElement("tr");
+                const trkey = document.createElement("td");
+                const rvalue = document.createElement("td");
+                trkey.textContent = i;
+                rvalue.textContent = bike[i];
+
+                tr.appendChild(trkey);
+                tr.appendChild(rvalue);
+
+                document.getElementById("id1").appendChild(tr);
+            }
+            break;
         case "mobile":
+            for (i in mobile) {
+                tr = document.createElement("tr");
+                const trkey = document.createElement("td");
+                const rvalue = document.createElement("td");
+                trkey.textContent = i;
+                rvalue.textContent = mobile[i];
+
+                tr.appendChild(trkey);
+                tr.appendChild(rvalue);
+
+                document.getElementById("id1").appendChild(tr);
+            }
+            break;
     }
 }
