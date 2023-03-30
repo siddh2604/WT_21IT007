c2db74c290e07330e313d6080089847f9ba24a85
Siddh Bhalodia <97431263+siddh2604@users.noreply.github.com>
2023-03-30 11:12:01 +0530

    Add files via upload

---
+++ lab5/Zoom.js
@@ -0,0 +1,10 @@
+function zoomIn() {
+    var image = document.getElementById("myImage");
+    image.style.width = "98%";
+    image.style.height = "98%";
+}
+function zoomOut() {
+    var image = document.getElementById("myImage");
+    image.style.width = "60%";
+    image.style.height = "60%";
+}
